edea0bc72694a05ee2352ec06aa05a25a3afb6de
darlyson rangel <darlysonrangel45@gmail.com>
2022-02-13 16:29:45 -0300

    Adicionado arquivo JavaScript, novas melhorias visuais e maior responsividade ao site

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onscroll = function(){
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+        document.querySelector("#nav-bar").className = "navbar navbar-expand-sm p-2 navbar-dark fixed-top"
+        document.querySelector("#title").style.fontSize = "30px"
+    }else{
+        document.querySelector("#nav-bar").className = "navbar navbar-expand-sm p-4 navbar-dark fixed-top"
+        document.querySelector("#title").style.fontSize = "30pt"
+    }
+}
+
+function isClicked() {
+    isExpanded = document.getElementById("toggle-button").getAttribute("aria-expanded")
+    if(isExpanded){
+        document.getElementById("navbarNav").style.position = "static"
+    }
+}
+window.onresize = function(){
+    if(window.innerWidth > 573){
+        document.getElementById("navbarNav").style.position = "absolute"
+    }
+}
